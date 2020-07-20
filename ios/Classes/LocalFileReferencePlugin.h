53be3591700bebf4a989a37987b6993a194af00d
Alex <alex.bunting314@gmail.com>
2020-07-21 08:22:40 +1000

    Remove platform channel code

--- ios/Classes/LocalFileReferencePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface LocalFileReferencePlugin : NSObject<FlutterPlugin>
-@end
--- ios/Classes/LocalFileReferencePlugin.m
+++
@@ -1,8 +0,0 @@
-#import "LocalFileReferencePlugin.h"
-#import <local_file_reference/local_file_reference-Swift.h>
-
-@implementation LocalFileReferencePlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftLocalFileReferencePlugin registerWithRegistrar:registrar];
-}
-@end
